c0eace1d6c3a6cc11349c97419bf5f6041e4c2d5
Donyell Margaret <donyell@donyell.nl>
2020-06-13 12:15:39 +0200

    Add navigation component for the main and detail screen

--- app/app/src/main/java/nl/donyell/m2mobi/MainActivity.kt
+++ app/app/src/main/java/nl/donyell/m2mobi/MainActivity.kt
@@ -1,11 +1,27 @@
 package nl.donyell.m2mobi
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import nl.donyell.m2mobi.response.GetPhotosResponse
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//            NetworkModule.service.getPhotos()
+//                .subscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe({ photosResponse ->
+//                    photosResponse.forEach { photo: GetPhotosResponse ->
+//                        Log.d("Main", photo.title)
+//                    }
+//                }, {
+//                    Log.d("Main", "wa3hed error: $it")
+//                })
     }
 }
